cda2c08993ae17090b022379fe1cac25f7293fac
Paul Visciano <140805500+paulvisciano@users.noreply.github.com>
2026-08-17 04:12:06 +0700

    Replace placeholder: i18n loader redirects to split locale files

--- i18n.js
+++ i18n.js
@@ -1,1 +1,10 @@
-PLACEHOLDER
+/* Locale packs are split for deploy size: load en/bg/es/de + core */
+(function(){
+  var s = ['/i18n-en.js','/i18n-bg.js','/i18n-es.js','/i18n-de.js','/i18n-core.js'];
+  s.forEach(function(src){
+    var el = document.createElement('script');
+    el.src = src;
+    el.defer = false;
+    document.head.appendChild(el);
+  });
+})();
