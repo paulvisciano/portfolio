6ddc8687decc74b7e132ada2edd331802e218c9e
Paul Visciano <140805500+paulvisciano@users.noreply.github.com>
2026-08-18 06:41:16 +0700

    Add Vercel Analytics to blog pages

---
+++ blog/analytics.js
@@ -0,0 +1,7 @@
+/* Vercel Web Analytics - load on all blog pages */
+(function () {
+  var s = document.createElement('script');
+  s.defer = true;
+  s.src = '/_vercel/insights/script.js';
+  document.head.appendChild(s);
+})();
